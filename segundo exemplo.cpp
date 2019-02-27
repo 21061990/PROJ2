72da1ccdaade68c00437281fb868ff81eb7501ef
21061990 <48037251+21061990@users.noreply.github.com>
2019-02-26 21:49:02 -0300

    Add files via upload

---
+++ segundo exemplo.cpp
@@ -0,0 +1,51 @@
+//codigo de linguagem em c
+
+#include <stdio.h>
+
+// programa ' le e imprime valor'
+
+int main()
+{
+	int A=0;
+	int B=0;
+	
+	printf("Informe um valor A inteiro:\n");
+	
+	scanf("%i",&A);
+	
+//	printf("O valor de A eh:%i \n",A);
+	
+	printf("informe um valor B inteiro: \n");
+	
+	scanf("%i",&B);
+	
+//	printf("O valor de B eh:%i \n",B);
+	
+	if (A>B)
+	{
+		 printf("\n\nA e maior que B! \n");
+	}
+	else
+	{
+		if(A>B)
+		{
+			printf("\n\nB e maior que A \n");
+		}
+		else
+		{
+			printf("\n\nB e igual a A! \n");
+		}
+	}
+	return 1;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
